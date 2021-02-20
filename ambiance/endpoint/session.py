e5b250db08f137ce644f849776f050aac6ef877c
= <=>
2021-02-20 01:53:04 -0500

    add skeleton endpoint

---
+++ ambiance/endpoint/session.py
@@ -0,0 +1,23 @@
+from dataclasses import field, dataclass
+from typing import List
+
+from dataclasses_json import dataclass_json
+
+from ambiance.endpoint.endpoint import endpoint, POST
+
+
+@dataclass_json
+@dataclass
+class CreateInput:
+    preferences: List[str] = field(default_factory=list)
+
+
+@dataclass_json
+@dataclass
+class CreateOutput:
+    session_id: str
+
+
+@endpoint(method=POST, body=CreateInput)
+def create(body: CreateInput, user: str) -> CreateOutput:
+    pass
--- handler.py
+++ handler.py
@@ -1,6 +1,8 @@
 from django.conf.urls import url
 from django.http import HttpRequest, JsonResponse
 
+from ambiance.endpoint import session
+
 DEBUG = True
 SECRET_KEY = "peepeepoopoojizzjazz"
 ROOT_URLCONF = __name__
@@ -10,4 +12,7 @@ def home(request: HttpRequest) -> JsonResponse:
     return JsonResponse({"message": "Hello world."})
 
 
-urlpatterns = [url(r"^$", home)]
+urlpatterns = [
+    url(r"^$", home),
+    url(r"^session/create$", session.create),
+]
