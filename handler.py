760414aed8b8d504091e2862c6debda7b7785467
Liam Stevenson <liam.m.stevenson@gmail.com>
2021-02-19 20:11:19 -0500

    Initial commit

---
+++ handler.py
@@ -0,0 +1,15 @@
+from django.conf.urls import url
+from django.http import HttpRequest, JsonResponse
+
+DEBUG = True
+SECRET_KEY = 'peepeepoopoojizzjazz'
+ROOT_URLCONF = __name__
+
+
+def home(request: HttpRequest) -> JsonResponse:
+    return JsonResponse({"message": "Hello world."})
+
+
+urlpatterns = [
+    url(r'^$', home),
+]
