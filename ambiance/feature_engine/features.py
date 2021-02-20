8ecd4861ec7533a7df30105ede852a0e3e88989a
Liam Stevenson <liam.m.stevenson@gmail.com>
2021-02-20 02:16:35 -0500

    Fix engine issues

--- ambiance/feature_engine/features.py
+++ ambiance/feature_engine/features.py
@@ -1,40 +1,51 @@
 import numpy as np
-from typing import Dict, List
-
-def vectorize_features(song: Dict[str, float]) -> tuple[str, ndarray]:
-  return {song["id"]: np.array([song["danceability"],
-    song["energy"],
-    song["key"],
-    song["loudness"],
-    song["mode"],
-    song["speechiness"],
-    song["acousticness"],
-    song["instrumentalness"],
-    song["liveness"],
-    song["valence"],
-    song["tempo"]
-  ])}
+from typing import Dict, List, Tuple, Any
 
 
-def average_features(library: List[tuple[str, ndarray]]) -> ndarray:
-  feature_sum = np.zeroes(11)
+def vectorize_features(song: Dict[str, Any]) -> Tuple[str, np.ndarray]:
+    return (
+        song["id"],
+        np.array(
+            [
+                song["danceability"],
+                song["energy"],
+                song["key"],
+                song["loudness"],
+                song["mode"],
+                song["speechiness"],
+                song["acousticness"],
+                song["instrumentalness"],
+                song["liveness"],
+                song["valence"],
+                song["tempo"],
+            ]
+        ),
+    )
 
-  feature_count = 0.
 
-  for song in library:
-  feature_sum += library[song]
-  feature_count += 1
+def average_features(library: List[Tuple[str, np.ndarray]]) -> np.ndarray:
+    feature_sum = np.zeros(11)
 
-  return feature_sum / feature_count
+    feature_count = 0.0
+
+    for song in library:
+        feature_sum += song[1]
+        feature_count += 1
+
+    return feature_sum / feature_count
 
 
-def euclidean_distance(first: ndarray, second: ndarray, scale: float=1.) -> float:
+def euclidean_distance(
+    first: np.ndarray, second: np.ndarray, scale: float = 1.0
+) -> float:
 
-  dist = np.copy(first)
-  dist -= second
-  dist = np.power(dist, 2.)
-  
-  return (numpy.sum(dist) ^ 0.5) * scale
+    dist = np.copy(first)
+    dist -= second
+    dist = np.power(dist, 2.0)
+    return (np.sum(dist) ^ 0.5) * scale
 
-def rank_library(library: List[tuple[str, ndarray]], features: ndarray) -> List[tuple[str, ndarray]]:
-  return sorted(library, key=lambda song: euclidean_distance(song[1], features))
+
+def rank_library(
+    library: List[Tuple[str, np.ndarray]], features: np.ndarray
+) -> List[Tuple[str, np.ndarray]]:
+    return sorted(library, key=lambda song: euclidean_distance(song[1], features))
