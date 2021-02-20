a0f164345d8d72aed1b93e21101917412308aed9
Liam Stevenson <liam.m.stevenson@gmail.com>
2021-02-20 09:38:02 -0500

    Support headers

--- ambiance/endpoint/endpoint.py
+++ ambiance/endpoint/endpoint.py
@@ -9,13 +9,12 @@ PUT = "PUT"
 
 
 def endpoint(
-    params: Any = None, body: Any = None, method: str = GET
+    params: Any = None, body: Any = None, header: Any = None, method: str = GET, auth_required: bool = True
 ) -> Callable[[Callable], Callable[[HttpRequest], HttpResponse]]:
     def endpoint_callback(
         endpoint_function: Callable,
     ) -> Callable[[HttpRequest], HttpResponse]:
         def inner(request: HttpRequest) -> HttpResponse:
-            print(request.method)
             if request.method == method:
                 if body is not None:
                     try:
@@ -38,8 +37,19 @@ def endpoint(
                 else:
                     decoded_url_params = None
 
+                if header is not None:
+                    try:
+                        header_dict = {
+                            key: val[0] for key, val in dict(request.headers).items()
+                        }
+                        decoded_header = params.from_dict(header_dict)
+                    except (JSONDecodeError, KeyError):
+                        return JsonResponse({"message": "Invalid header"}, status=400)
+                else:
+                    decoded_header = None
+
                 result = endpoint_function(
-                    params=decoded_url_params, body=decoded_body, user="USER_ID"
+                    params=decoded_url_params, body=decoded_body, header=decoded_header, user="USER_ID"
                 ).to_dict()
                 return JsonResponse(result)
             else:
