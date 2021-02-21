3f953d5b9e5424cb7e9a9fc99c3c0a509ff93805
Liam Stevenson <liam.m.stevenson@gmail.com>
2021-02-20 22:31:19 -0500

    Fix missing change

--- ambiance/helpers/user_info.py
+++ ambiance/helpers/user_info.py
@@ -6,8 +6,8 @@ from ambiance.model import db
 
 
 def get_user(user_id: str) -> Tuple[str, str]:
-    sp = db.DB.users[user_id].spotipy
+    sp = db.DB().users[user_id].spotipy
     return sp.me()
 
 
-print(get_user(""))
+# print(get_user(""))
